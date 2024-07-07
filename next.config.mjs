827eefee64349a6ee0bc85aea04d56fc03edc836
Ritesh <riteshpatel246@gmail.com>
2024-07-07 14:04:42 +0530

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    suppressHydrationWarning: true,
+  },
+  swcMinify: true,
+};
 
 export default nextConfig;
