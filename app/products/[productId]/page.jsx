bebecbe71bd75483b87af8f459a9201ed3c6b2a3
Ritesh <riteshpatel246@gmail.com>
2024-07-10 11:50:37 +0530

    product page fix

--- app/products/[productId]/page.jsx
+++ app/products/[productId]/page.jsx
@@ -24,7 +24,7 @@ const Page = () => {
   return (
     <>
       <Header navItems={mainNav} isSearch={true} />
-      <div className="bg-gray-50 mt-16 min-h-screen py-8">
+      <div className="bg-gray-50 mt-12 md:mt-24 min-h-screen py-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
             <motion.div
